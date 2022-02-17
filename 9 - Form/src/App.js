f434eb9998f27f05467383afabb0a5f09f6e0092
Czerwcuu <czerwiecr1999@gmail.com>
2022-02-18 00:04:22 +0100

    9 - Form JSX

---
+++ 9 - Form/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+import User from "./User/User";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="Red-color">Aplikacja React - JSX</h1>
+        <div className="Form-container">
+          <label className="Form-label">Username</label>
+          <input className="Form-input" type="text" id="username" />
+          <label className="Form-label">Role</label>
+          <input className="Form-input" type="text" id="role" />
+          <label className="Form-label">Active</label>
+          <input className="Form-input" type="checkbox" id="active" />
+          <button className="Form-button" >Show user</button>
+        </div>
+        <User username="Paul" role="Miner">
+          active
+        </User>
+        <User username="Mike" role="Farmer">
+          inactive
+        </User>
+      </div>
+      //React.createElement('div',{className: 'App'},React.createElement('h1',{className: 'Red-color'},'Aplikacja React - createElement'))
+    );
+  }
+}
+
+export default App;
